61a034c0f6362f18c7f00f12ba43b953e11350fd
fevico <ajayifavour81@gmail.com>
2025-06-18 20:57:18 +0100

    cors error fixed

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,25 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({ origin: [process.env.APP_URL!], credentials: true }));
+// app.use(cors({ origin: [process.env.APP_URL!], credentials: true }));
+
+const allowedOrigins = [
+    'https://blog-app-p44d.vercel.app',
+    process.env.APP_URL?.replace(/\/$/, ''),
+  ].filter(Boolean) as string[];
+    
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else { 
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true,
+    })
+  );
 
 app.get("/", (req, res) => { 
     res.send("Hello World") 
